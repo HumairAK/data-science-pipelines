fbd0f0ce4ea5cd9d373da9aa50029c1a5203c97d
Humair Khan <HumairAK@users.noreply.github.com>
2025-06-14 09:31:06 -0400

    changes
    Signed-off-by: Humair Khan <HumairAK@users.noreply.github.com>

---
+++ backend/src/v2/metadata_provider/mlflow/factory.go
@@ -0,0 +1,25 @@
+package mlflow
+
+import (
+	"github.com/kubeflow/pipelines/backend/src/apiserver/common"
+	"github.com/kubeflow/pipelines/backend/src/apiserver/storage"
+	"github.com/kubeflow/pipelines/backend/src/v2/metadata_provider"
+)
+
+type mlflowFactory struct{}
+
+func (f *mlflowFactory) NewExperimentStore(cfg common.UnstructuredJSON) (storage.ExperimentStoreInterface, error) {
+	return NewExperimentStore(cfg)
+}
+
+func (f *mlflowFactory) NewRunProvider(cfg common.UnstructuredJSON) (metadata_provider.RunProvider, error) {
+	return NewRunsProvider(cfg)
+}
+
+func (f *mlflowFactory) NewMetadataArtifactProvider(cfg common.UnstructuredJSON) (metadata_provider.MetadataArtifactProvider, error) {
+	return NewArtifactsProvider(cfg)
+}
+
+func init() {
+	metadata_provider.Register("mlflow", &mlflowFactory{})
+}
