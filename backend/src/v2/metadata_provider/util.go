c092828cc4a1897feb78c9b8c99f29f0ea43604a
Humair Khan <HumairAK@users.noreply.github.com>
2025-06-22 16:34:36 -0400

    changes
    Signed-off-by: Humair Khan <HumairAK@users.noreply.github.com>

--- backend/src/v2/metadata_provider/util.go
+++ backend/src/v2/metadata_provider/util.go
@@ -1,6 +1,8 @@
 package metadata_provider
 
 import (
+	"fmt"
+	"google.golang.org/protobuf/encoding/protojson"
 	"google.golang.org/protobuf/types/known/structpb"
 	"sync"
 )
@@ -28,7 +30,29 @@ func Lookup(name string) (ProviderFactory, bool) {
 func PBParamsToRunParameters(inputParams map[string]*structpb.Value) []RunParameter {
 	var parameters []RunParameter
 	for k, v := range inputParams {
-		parameters = append(parameters, RunParameter{Name: k, Value: v.GetStringValue()})
+		parameters = append(parameters, RunParameter{Name: k, Value: structPBtoString(v)})
 	}
 	return parameters
+}
+
+// In the case of list/struct we return json values
+func structPBtoString(v *structpb.Value) string {
+	switch kind := v.Kind.(type) {
+	case *structpb.Value_NullValue:
+		return "null"
+	case *structpb.Value_NumberValue:
+		return fmt.Sprintf("%v", kind.NumberValue)
+	case *structpb.Value_StringValue:
+		return kind.StringValue
+	case *structpb.Value_BoolValue:
+		return fmt.Sprintf("%v", kind.BoolValue)
+	case *structpb.Value_StructValue:
+		b, _ := protojson.Marshal(kind.StructValue)
+		return string(b)
+	case *structpb.Value_ListValue:
+		b, _ := protojson.Marshal(kind.ListValue)
+		return string(b)
+	default:
+		return "<unknown>"
+	}
 }
