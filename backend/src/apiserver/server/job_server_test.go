293d2c19ab26b96d7e366297dcbcfe0b8c1ccbcc
Humair Khan <HumairAK@users.noreply.github.com>
2025-07-17 00:36:37 -0400

    update job_server_test
    Signed-off-by: Humair Khan <HumairAK@users.noreply.github.com>

--- backend/src/apiserver/server/job_server_test.go
+++ backend/src/apiserver/server/job_server_test.go
@@ -102,10 +102,32 @@ var (
 	}
 )
 
+func createJobServerV1(resourceManager *resource.ResourceManager) *JobServerV1 {
+	return &JobServerV1{
+		BaseJobServer: &BaseJobServer{
+			resourceManager: resourceManager,
+			options: &JobServerOptions{
+				CollectMetrics: false,
+			},
+		},
+	}
+}
+
+func createJobServer(resourceManager *resource.ResourceManager) *JobServer {
+	return &JobServer{
+		BaseJobServer: &BaseJobServer{
+			resourceManager: resourceManager,
+			options: &JobServerOptions{
+				CollectMetrics: false,
+			},
+		},
+	}
+}
+
 func TestCreateJob_WrongInput(t *testing.T) {
 	clients, manager, experiment, _ := initWithExperimentAndPipelineVersion(t)
 	defer clients.Close()
-	server := NewJobServer(manager, &JobServerOptions{CollectMetrics: false})
+	server := createJobServerV1(manager)
 	tests := []struct {
 		name   string
 		arg    *apiv1beta1.Job
@@ -254,7 +276,7 @@ func TestCreateJob_WrongInput(t *testing.T) {
 func TestCreateJob_pipelineVersion(t *testing.T) {
 	clients, manager, exp, pipelineVersion := initWithExperimentAndPipelineVersion(t)
 	defer clients.Close()
-	server := NewJobServer(manager, &JobServerOptions{CollectMetrics: false})
+	server := createJobServerV1(manager)
 	rr := []*apiv1beta1.ResourceReference{
 		{
 			Key: &apiv1beta1.ResourceKey{
@@ -324,7 +346,7 @@ func TestCreateJob_NoResRefs(t *testing.T) {
 	defer clients.Close()
 	clients.UpdateUUID(util.NewFakeUUIDGeneratorOrFatal(DefaultFakeIdTwo, nil))
 	manager = resource.NewResourceManager(clients, &resource.ResourceManagerOptions{CollectMetrics: false})
-	server := NewJobServer(manager, &JobServerOptions{CollectMetrics: false})
+	server := createJobServerV1(manager)
 	apiJob := &apiv1beta1.Job{
 		Name:           "job1",
 		Enabled:        true,
@@ -387,7 +409,7 @@ func TestCreateJob_NoResRefs(t *testing.T) {
 func TestCreateJob(t *testing.T) {
 	clients, manager, _ := initWithExperiment(t)
 	defer clients.Close()
-	server := NewJobServer(manager, &JobServerOptions{CollectMetrics: false})
+	server := createJobServerV1(manager)
 	job, err := server.CreateJob(nil, &apiv1beta1.CreateJobRequest{Job: commonApiJob})
 	assert.Nil(t, err)
 	matched := 0
@@ -412,7 +434,7 @@ func TestCreateJob(t *testing.T) {
 func TestCreateJob_V2(t *testing.T) {
 	clients, manager, _ := initWithExperiment(t)
 	defer clients.Close()
-	server := NewJobServer(manager, &JobServerOptions{CollectMetrics: false})
+	server := createJobServerV1(manager)
 
 	listParams := []interface{}{1, 2, 3}
 	v2RuntimeListParams, _ := structpb.NewList(listParams)
@@ -515,7 +537,7 @@ func TestListRecurringRuns_MultiUser(t *testing.T) {
 
 	clients, manager, experiment := initWithExperiment(t)
 	defer clients.Close()
-	server := NewJobServer(manager, &JobServerOptions{CollectMetrics: false})
+	server := createJobServer(manager)
 
 	pipelineSpecStruct := &structpb.Struct{}
 	yaml.Unmarshal([]byte(v2SpecHelloWorld), pipelineSpecStruct)
@@ -598,7 +620,7 @@ func TestCreateJob_Unauthorized(t *testing.T) {
 	clients, manager, _ := initWithExperiment_SubjectAccessReview_Unauthorized(t)
 	defer clients.Close()
 
-	server := NewJobServer(manager, &JobServerOptions{CollectMetrics: false})
+	server := createJobServerV1(manager)
 	_, err := server.CreateJob(ctx, &apiv1beta1.CreateJobRequest{Job: commonApiJob})
 	assert.NotNil(t, err)
 	assert.Contains(
@@ -619,13 +641,13 @@ func TestGetJob_Unauthorized(t *testing.T) {
 	clients, manager, _ := initWithExperiment(t)
 	defer clients.Close()
 
-	server := NewJobServer(manager, &JobServerOptions{CollectMetrics: false})
+	server := createJobServerV1(manager)
 	job, err := server.CreateJob(ctx, &apiv1beta1.CreateJobRequest{Job: commonApiJob})
 	assert.Nil(t, err)
 
 	clients.SubjectAccessReviewClientFake = client.NewFakeSubjectAccessReviewClientUnauthorized()
 	manager = resource.NewResourceManager(clients, &resource.ResourceManagerOptions{CollectMetrics: false})
-	server = NewJobServer(manager, &JobServerOptions{CollectMetrics: false})
+	server = createJobServerV1(manager)
 
 	_, err = server.GetJob(ctx, &apiv1beta1.GetJobRequest{Id: job.Id})
 	assert.NotNil(t, err)
@@ -645,7 +667,7 @@ func TestGetJob_Multiuser(t *testing.T) {
 
 	clients, manager, _ := initWithExperiment(t)
 	defer clients.Close()
-	server := NewJobServer(manager, &JobServerOptions{CollectMetrics: false})
+	server := createJobServerV1(manager)
 	createdJob, err := server.CreateJob(ctx, &apiv1beta1.CreateJobRequest{Job: commonApiJob})
 	assert.Nil(t, err)
 
@@ -681,7 +703,7 @@ func TestListJobs_Unauthorized(t *testing.T) {
 	clients, manager, experiment := initWithExperiment_SubjectAccessReview_Unauthorized(t)
 	defer clients.Close()
 
-	server := NewJobServer(manager, &JobServerOptions{CollectMetrics: false})
+	server := createJobServerV1(manager)
 	_, err := server.ListJobs(ctx, &apiv1beta1.ListJobsRequest{
 		ResourceReferenceKey: &apiv1beta1.ResourceKey{
 			Type: apiv1beta1.ResourceType_EXPERIMENT,
@@ -718,7 +740,7 @@ func TestListJobs_Multiuser(t *testing.T) {
 
 	clients, manager, _ := initWithExperiment(t)
 	defer clients.Close()
-	server := NewJobServer(manager, &JobServerOptions{CollectMetrics: false})
+	server := createJobServerV1(manager)
 	_, err := server.CreateJob(ctx, &apiv1beta1.CreateJobRequest{Job: commonApiJob})
 	assert.Nil(t, err)
 
@@ -826,13 +848,13 @@ func TestEnableJob_Unauthorized(t *testing.T) {
 
 	clients, manager, _ := initWithExperiment(t)
 	defer clients.Close()
-	server := NewJobServer(manager, &JobServerOptions{CollectMetrics: false})
+	server := createJobServerV1(manager)
 	job, err := server.CreateJob(ctx, &apiv1beta1.CreateJobRequest{Job: commonApiJob})
 	assert.Nil(t, err)
 
 	clients.SubjectAccessReviewClientFake = client.NewFakeSubjectAccessReviewClientUnauthorized()
 	manager = resource.NewResourceManager(clients, &resource.ResourceManagerOptions{CollectMetrics: false})
-	server = NewJobServer(manager, &JobServerOptions{CollectMetrics: false})
+	server = createJobServerV1(manager)
 
 	_, err = server.EnableJob(ctx, &apiv1beta1.EnableJobRequest{Id: job.Id})
 	assert.NotNil(t, err)
@@ -852,7 +874,7 @@ func TestEnableJob_Multiuser(t *testing.T) {
 
 	clients, manager, _ := initWithExperiment(t)
 	defer clients.Close()
-	server := NewJobServer(manager, &JobServerOptions{CollectMetrics: false})
+	server := createJobServerV1(manager)
 
 	job, err := server.CreateJob(ctx, &apiv1beta1.CreateJobRequest{Job: commonApiJob})
 	assert.Nil(t, err)
@@ -871,13 +893,13 @@ func TestDisableJob_Unauthorized(t *testing.T) {
 
 	clients, manager, _ := initWithExperiment(t)
 	defer clients.Close()
-	server := NewJobServer(manager, &JobServerOptions{CollectMetrics: false})
+	server := createJobServerV1(manager)
 	job, err := server.CreateJob(ctx, &apiv1beta1.CreateJobRequest{Job: commonApiJob})
 	assert.Nil(t, err)
 
 	clients.SubjectAccessReviewClientFake = client.NewFakeSubjectAccessReviewClientUnauthorized()
 	manager = resource.NewResourceManager(clients, &resource.ResourceManagerOptions{CollectMetrics: false})
-	server = NewJobServer(manager, &JobServerOptions{CollectMetrics: false})
+	server = createJobServerV1(manager)
 
 	_, err = server.DisableJob(ctx, &apiv1beta1.DisableJobRequest{Id: job.Id})
 	assert.NotNil(t, err)
@@ -897,7 +919,7 @@ func TestDisableJob_Multiuser(t *testing.T) {
 
 	clients, manager, _ := initWithExperiment(t)
 	defer clients.Close()
-	server := NewJobServer(manager, &JobServerOptions{CollectMetrics: false})
+	server := createJobServerV1(manager)
 
 	job, err := server.CreateJob(ctx, &apiv1beta1.CreateJobRequest{Job: commonApiJob})
 	assert.Nil(t, err)
@@ -916,7 +938,7 @@ func TestListJobs_Unauthenticated(t *testing.T) {
 	clients, manager, experiment := initWithExperiment(t)
 	defer clients.Close()
 
-	server := NewJobServer(manager, &JobServerOptions{CollectMetrics: false})
+	server := createJobServerV1(manager)
 	_, err := server.ListJobs(ctx, &apiv1beta1.ListJobsRequest{
 		ResourceReferenceKey: &apiv1beta1.ResourceKey{
 			Type: apiv1beta1.ResourceType_EXPERIMENT,
@@ -947,7 +969,7 @@ func TestListJobs_Unauthenticated(t *testing.T) {
 func TestCreateRecurringRun(t *testing.T) {
 	clients, manager, _ := initWithExperiment(t)
 	defer clients.Close()
-	server := NewJobServer(manager, &JobServerOptions{CollectMetrics: false})
+	server := createJobServer(manager)
 
 	pipelineSpecStruct := &structpb.Struct{}
 	yaml.Unmarshal([]byte(v2SpecHelloWorld), pipelineSpecStruct)
@@ -1009,7 +1031,7 @@ func TestCreateRecurringRun(t *testing.T) {
 func TestGetRecurringRun(t *testing.T) {
 	clients, manager, _ := initWithExperiment(t)
 	defer clients.Close()
-	server := NewJobServer(manager, &JobServerOptions{CollectMetrics: false})
+	server := createJobServer(manager)
 
 	pipelineSpecStruct := &structpb.Struct{}
 	yaml.Unmarshal([]byte(v2SpecHelloWorld), pipelineSpecStruct)
@@ -1074,7 +1096,7 @@ func TestGetRecurringRun(t *testing.T) {
 func TestListRecurringRuns(t *testing.T) {
 	clients, manager, experiment := initWithExperiment(t)
 	defer clients.Close()
-	server := NewJobServer(manager, &JobServerOptions{CollectMetrics: false})
+	server := createJobServer(manager)
 
 	pipelineSpecStruct := &structpb.Struct{}
 	yaml.Unmarshal([]byte(v2SpecHelloWorld), pipelineSpecStruct)
@@ -1152,7 +1174,7 @@ func TestListRecurringRuns(t *testing.T) {
 func TestEnableRecurringRun(t *testing.T) {
 	clients, manager, _ := initWithExperiment(t)
 	defer clients.Close()
-	server := NewJobServer(manager, &JobServerOptions{CollectMetrics: false})
+	server := createJobServer(manager)
 
 	pipelineSpecStruct := &structpb.Struct{}
 	yaml.Unmarshal([]byte(v2SpecHelloWorld), pipelineSpecStruct)
@@ -1187,7 +1209,7 @@ func TestEnableRecurringRun(t *testing.T) {
 func TestDisableRecurringRun(t *testing.T) {
 	clients, manager, _ := initWithExperiment(t)
 	defer clients.Close()
-	server := NewJobServer(manager, &JobServerOptions{CollectMetrics: false})
+	server := createJobServer(manager)
 
 	pipelineSpecStruct := &structpb.Struct{}
 	yaml.Unmarshal([]byte(v2SpecHelloWorld), pipelineSpecStruct)
